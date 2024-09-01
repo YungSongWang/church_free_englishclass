5fd7800a57dc5c5e8655ee15baec0a49ec7fd01e
Cloude <yunsong2023uk@163.com>
2024-09-01 19:08:10 +0930

    free_english_class_vision4
    change the logo

--- fix_color.py
+++ fix_color.py
@@ -1,11 +1,11 @@
 import re
 
 # 打开原始SVG文件
-with open('assets/images/church-logo.svg', 'r') as file:
+with open('assets/images/english_logo_white.svg', 'r') as file:
     svg_content = file.read()
 
 # 将所有黑色填充改为白色
-svg_content_updated = re.sub(r'fill="#212225"|fill="black"', 'fill="#FFFFFF"', svg_content)
+svg_content_updated = re.sub(r'fill="#000000"|fill="black"', 'fill="#FFFFFF"', svg_content)
 
 # 将更新后的内容写入新文件或覆盖原文件
 with open('assets/images/church-logo.svg', 'w') as file:
